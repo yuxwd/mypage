0327818914286c8a23304cae8d6aa7755a667279
yuxwd <yuxiang.ji@outlook.com>
2024-11-01 16:12:57 +0800

    简单进行文件管理

--- index.js
+++ index.js
@@ -8,29 +8,9 @@ app.use(express.urlencoded({ extended: true }));  // 解析URL编码的请求
 
 // 定义路由  
 app.get('/', (req, res) => {  
-  res.send('欢迎来到首页！');  
+  res.send(./src/index.html);  
 });  
 
-app.get('/about', (req, res) => {  
-  res.send('关于我们页面');  
+app.get('/demo', (req, res) => {  
+  res.send(./src/index.html);  
 });  
-
-// 动态路由示例  
-app.get('/users/:id', (req, res) => {  
-  const userId = req.params.id;  
-  res.send(`用户ID是：${userId}`);  
-});  
-
-// POST 请求示例  
-app.post('/users', (req, res) => {  
-  const user = req.body;  
-  res.send(`新用户创建：${JSON.stringify(user)}`);  
-});  
-
-// 启动服务器  
-app.listen(PORT, () => {  
-  console.log(`服务器正在运行于 http://localhost:${PORT}`);  
-});
-window.onload = function () {
-  document.getElementById('main-content').innerHTML = 'Github Page demo,lightearth love you!'
-}
